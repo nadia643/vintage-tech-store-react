45dbc7fc06cb4765f56221810aa7a746f3eb815c
John Doe <nadia643@gmail.com>
2020-05-11 13:28:02 +0100

    added alerts for card payment succeeding or failing in checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -3,10 +3,10 @@ import { CartContext } from "../context/cart";
 import { UserContext } from "../context/user";
 import { useHistory } from "react-router-dom";
 import EmptyCart from "../components/Cart/EmptyCart";
-//react-stripe-elements
+import { CardElement, StripeProvider, Elements, injectStripe } from "react-stripe-elements";
 import submitOrder from "../strapi/submitOrder";
  
-export default function Checkout(props) {
+function Checkout(props) {
   const { cart, total, clearCart } = useContext(CartContext);
   const { user, showAlert, hideAlert, alert } = useContext(UserContext);
   const history = useHistory();
@@ -17,7 +17,39 @@ export default function Checkout(props) {
   const isEmpty = !name || alert.show;
 
   async function handleSubmit(e) {
+    showAlert({ msg: "Submitting order.... please wait" });
     e.preventDefault();
+    const response = await props.stripe
+      .createToken()
+      .catch(error => console.log(error));
+
+      const{ token } = response;
+      if(token) {
+        setError('');
+        const { id } = token;
+        let order = await submitOrder({ 
+          name: name, 
+          total: total, 
+          items: cart,
+          stripeTokenId: id,
+          userToken: user.token
+        })
+
+      if(order) {
+        showAlert({ msg: "Your order is complete" });
+        clearCart();
+        // history.push - navigate to home page once order is complete
+        history.push("/");
+        return;
+      }
+      else {
+        showAlert({ msg: "There was an error with your order. Please try again!", type: "danger" })
+      }
+      } else {
+        hideAlert();
+        setError(response.error.message);
+      }
+      
   }
 
   if(cart.length < 1) return <EmptyCart />;
@@ -54,6 +86,7 @@ export default function Checkout(props) {
       {/* end of cart element */}
 
       {/* STRIPE ELEMENTS */}
+      <CardElement className="card-element"></CardElement>
       {/* stripe errors */}
       {error && <p className="form-empty">{error}</p>}
 
@@ -73,3 +106,17 @@ export default function Checkout(props) {
     </form>
   </section>
 }
+
+const CardForm = injectStripe(Checkout);
+
+const StripeWrapper = () => {
+  return (
+    <StripeProvider apiKey="pk_test_QmVxkiLSAPPJmpssTLBH410M00zxyZjeyW">
+      <Elements>
+        <CardForm></CardForm>
+      </Elements>
+    </StripeProvider>
+  )
+}
+
+export default StripeWrapper;
