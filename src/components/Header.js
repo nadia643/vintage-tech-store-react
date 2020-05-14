dbf64e9442acc7be51a9e64de7e6de8df6b63580
John Doe <nadia643@gmail.com>
2020-05-14 14:07:21 +0100

    adding right code

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
       <ul>
         <div>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">Home Page</Link>
           </li>
           <li>
             <Link to="/about">About</Link>
